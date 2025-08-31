3d43f934897b3d6c6da66c361c9496996031ec7e
Bhxvishya <bhavishy.sharma@gmail.com>
2025-09-01 01:22:34 +0400

    Use Vercel Remix adapter with stable versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
   plugins: [
     remix({
+      presets: [vercelPreset()],
       ignoredRouteFiles: ["**/.*"],
     }),
   ],
